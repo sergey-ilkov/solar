16443a021e868bda076c76af5608fe859db1160d
sergey-ilkov <72069299+sergey-ilkov@users.noreply.github.com>
2022-02-01 12:58:02 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,88 @@
+$(function () {
+    $('.info-tabs__item').on('click', function (e) {
+        e.preventDefault();
+
+        $('.info-tabs__item').removeClass('info-tabs__item--active');
+        $(this).addClass('info-tabs__item--active');
+
+        $('.info-tabs__content').removeClass('info-tabs__content--active');
+        let id = $(this).attr('href');
+        $(id).addClass('info-tabs__content--active');
+    });
+    $('.info-slider').slick({
+        fade: true,
+        prevArrow:
+            '<svg class="slick-left" width="41" height="8" viewBox="0 0 41 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.646446 3.64645C0.451183 3.84171 0.451183 4.15829 0.646446 4.35355L3.82843 7.53553C4.02369 7.7308 4.34027 7.7308 4.53553 7.53553C4.7308 7.34027 4.7308 7.02369 4.53553 6.82843L1.70711 4L4.53553 1.17157C4.7308 0.976311 4.7308 0.659728 4.53553 0.464466C4.34027 0.269204 4.02369 0.269204 3.82843 0.464466L0.646446 3.64645ZM41 3.5L1 3.5V4.5L41 4.5V3.5Z" fill="#303030"/></svg>',
+        nextArrow:
+            '<svg class="slick-right" width="41" height="8" viewBox="0 0 41 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M40.3536 4.35355C40.5488 4.15829 40.5488 3.84171 40.3536 3.64645L37.1716 0.464466C36.9763 0.269204 36.6597 0.269204 36.4645 0.464466C36.2692 0.659728 36.2692 0.976311 36.4645 1.17157L39.2929 4L36.4645 6.82843C36.2692 7.02369 36.2692 7.34027 36.4645 7.53553C36.6597 7.7308 36.9763 7.7308 37.1716 7.53553L40.3536 4.35355ZM0 4.5H40V3.5H0V4.5Z" fill="#303030"/></svg>',
+    });
+    $('.clients-slider').slick({
+        prevArrow:
+            '<svg class="slick-left" width="41" height="8" viewBox="0 0 41 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.646446 3.64645C0.451183 3.84171 0.451183 4.15829 0.646446 4.35355L3.82843 7.53553C4.02369 7.7308 4.34027 7.7308 4.53553 7.53553C4.7308 7.34027 4.7308 7.02369 4.53553 6.82843L1.70711 4L4.53553 1.17157C4.7308 0.976311 4.7308 0.659728 4.53553 0.464466C4.34027 0.269204 4.02369 0.269204 3.82843 0.464466L0.646446 3.64645ZM41 3.5L1 3.5V4.5L41 4.5V3.5Z" fill="#303030"/></svg>',
+        nextArrow:
+            '<svg class="slick-right" width="41" height="8" viewBox="0 0 41 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M40.3536 4.35355C40.5488 4.15829 40.5488 3.84171 40.3536 3.64645L37.1716 0.464466C36.9763 0.269204 36.6597 0.269204 36.4645 0.464466C36.2692 0.659728 36.2692 0.976311 36.4645 1.17157L39.2929 4L36.4645 6.82843C36.2692 7.02369 36.2692 7.34027 36.4645 7.53553C36.6597 7.7308 36.9763 7.7308 37.1716 7.53553L40.3536 4.35355ZM0 4.5H40V3.5H0V4.5Z" fill="#303030"/></svg>',
+    });
+
+    // JavaScript
+    // scroll
+
+    function scrollTo(element) {
+        window.scroll({
+            left: 0,
+            top: element.offsetTop,
+            behavior: 'smooth',
+        });
+    }
+
+    const headerBtn = document.querySelector('.header__button');
+    const clientsBtn = document.querySelector('.clients__btn');
+    const footer = document.querySelector('.footer');
+
+    headerBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        scrollTo(footer);
+    });
+    clientsBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        scrollTo(footer);
+    });
+
+    /* ANIMATION */
+
+    const animItems = document.querySelectorAll('._anim-items');
+
+    if (animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll() {
+            for (let i = 0; i < animItems.length; i++) {
+                const animItem = animItems[i];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+
+                if (scrollY > animItemOffset - animItemPoint && scrollY < animItemOffset + animItemHeight) {
+                    animItem.classList.add('_active');
+                } else {
+                    if (!animItem.classList.contains('_anim-no-hide')) {
+                        animItem.classList.remove('_active');
+                    }
+                }
+            }
+        }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+        }
+
+        setTimeout(() => {
+            animOnScroll();
+        }, 300);
+    }
+});
